b09f13a7c6e8b2352c56fd70c5e4ada56680fe47
Florin <albuflorin929@yahoo.co.uk>
2026-05-09 12:24:50 +0100

    Add functionality javascript for navbar hamburger, added navbar.js and added for every each page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
+// const hamburger = document.getElementById('hamburger');
+// const navMenu = document.getElementById('nav-menu');
 
-hamburger.addEventListener('click', () => {
-  const isOpen = navMenu.classList.toggle('open');
-  hamburger.setAttribute('aria-expanded', isOpen);
-  hamburger.setAttribute('aria-label', isOpen ? 'Close menu' : 'Open menu');
-});
+// hamburger.addEventListener('click', () => {
+//   const isOpen = navMenu.classList.toggle('open');
+//   hamburger.setAttribute('aria-expanded', isOpen);
+//   hamburger.setAttribute('aria-label', isOpen ? 'Close menu' : 'Open menu');
+// });

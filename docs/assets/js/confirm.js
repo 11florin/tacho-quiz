844f80d876861d298a81e74ee7204f6ef21e21a3
Florin <albuflorin929@yahoo.co.uk>
2026-06-20 15:58:37 +0100

    Add Personalize welome message

--- docs/assets/js/confirm.js
+++ docs/assets/js/confirm.js
@@ -1,11 +1,17 @@
-"use strict"
+"use strict";
 
 // Read username saved during login
 const user = localStorage.getItem("tq_user");
 
 // Defensive design: if no username, redirect to login
 if (!user) {
-    alert("No login session found. Please log in first.");
-    window.location.href = "login.html";
-    throw new Error("No username found");
+  alert("No login session found. Please log in first.");
+  window.location.href = "login.html";
+  throw new Error("No username found");
 }
+// Personalize welcome message
+const title = document.getElementById("welcome-title");
+title.textContent = `Welcome ${user}`;
+
+const text = document.getElementById("confirm-text");
+text.textContent = `${user}, your login was successful.`;

d588e107b9c84c543007e958a43170ae415a5459
Florin <albuflorin929@yahoo.co.uk>
2026-06-20 15:35:56 +0100

    Add login.js for functionality and validation, add variabile for DOM  manipulation

---
+++ docs/assets/js/login.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const form = document.querySelector("form");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const errorMsg = document.getElementById("error-msg");
+
+// Hide error message at the begin 
+errorMsg.style.display = "none";
+
